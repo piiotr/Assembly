d5273fddb49f7eaac8c48b0dc3dabed061c72314
piiotr <ptrmkaminski@gmail.com>
2023-11-11 13:29:51 +0100

    bill L19

--- Bill_Assembly/Linux/Projects/ARM32_SimpleCalc/main.asm
+++ Bill_Assembly/Linux/Projects/ARM32_SimpleCalc/main.asm
@@ -1,4 +1,6 @@
 .section .data
+    dWordVar1:   .dword      0x100
+    dWordVar2:   .dword      0x200
 .section .bss
 .section .text
 .global _start
@@ -10,7 +12,26 @@
         mov r0, #100
         add r0, r0, #200
 
+        sub r0, r0, #300
+        sub r0, r0, r1
 
+        ldr r3, =dWordVar1
+        ldr r0, [r3]
+
+        ldr r3, =dWordVar2
+        ldr r1, [r3]
+
+        adds r0, r0, r1        
+
+        mov r0, #0xffffffff
+        add r0, r0, #1
+        bcs printerror
+        b   endall
+
+    printerror:
+        mov r0, #1
+
+    endall:
         mov r0, #0 		@Place 0 into the r0 register, Return code
         mov r7, #1 		@Place 1 into r7, Exit the program
         svc #0			@Poke the kernel
