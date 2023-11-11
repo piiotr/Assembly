e849ff651d122a250b75766f564a90b59259e5aa
piiotr <ptrmkaminski@gmail.com>
2023-11-11 19:37:54 +0100

    linux - arrays intro

--- Bill_Assembly/Linux/Projects/x64_Array1/main.asm
+++ Bill_Assembly/Linux/Projects/x64_Array1/main.asm
@@ -1,8 +1,24 @@
 section .data
 	byteArray	db	11h, 22h, 33h, 44h, 55h
+		.TYPE	equ		1
+		.SIZEOF equ 	$-byteArray
+		.LENGTHOF equ	byteArray.SIZEOF / byteArray.TYPE	 
+
 	wordArray	dw	6666h, 7777h, 8888h, 9999h
+		.TYPE 	equ 	2
+		.SIZEOF	equ 	$-wordArray
+		.LENGTHOF equ	wordArray.SIZEOF / wordArray.TYPE	 
+	
 	dwordArray	dd	0bbbbbbbbh, 0cccccccch, 0ddddddddh
+		.TYPE	equ		4
+		.SIZEOF	equ		$-dwordArray
+		.LENGTHOF equ	dwordArray.SIZEOF / dwordArray.TYPE	 
+	
 	qwordArray	dq	0eeeeeeeeeeeeeeeeh, 0ffffffffffffffffh, 0h, 1111h
+		.TYPE	equ		8
+		.SIZEOF	equ		$-qwordArray
+		.LENGTHOF equ	qwordArray.SIZEOF / qwordArray.TYPE	 
+
 section .bss
 section .text
 global  _start 
@@ -10,24 +26,28 @@ global  _start
 		mov		rax,	0h							
 		mov		rsi,	0h							
 		
-		mov		rsi,	OFFSET	byteArray			
-		movzx	rax,	BYTE PTR [rsi]				
-		add		rsi,	TYPE	byteArray			 
-		movzx	rax,	BYTE PTR [rsi]				
+		mov		rsi,	byteArray			
+		movzx	rax,	BYTE [rsi]				
+		add		rsi,	byteArray.TYPE			 
+		movzx	rax,	BYTE [rsi]				
 
-		mov		rsi,	OFFSET	wordArray
-		movzx	rax,	WORD PTR [rsi]
-		add		rsi,	TYPE	wordArray
-		movzx	rax,	WORD PTR [rsi]
+		mov		rsi,	wordArray
+		movzx	rax,	WORD [rsi]
+		add		rsi,	wordArray.TYPE
+		movzx	rax,	WORD [rsi]
 
-		mov		rsi,	OFFSET	qwordArray
+		mov		rsi,	qwordArray
 		mov		rax,	[rsi]						
-		add		rsi,	TYPE	qwordArray
+		add		rsi,	qwordArray.TYPE
 		mov		rax,	[rsi]
 
-		mov		rax,	SIZEOF		qwordArray
-		mov		rax,	LENGTHOF	qwordArray
+		mov		rax,	qwordArray.SIZEOF
+		mov		rsi,	qwordArray
+		mov		rax,	[rsi]
+		add		rsi,	qwordArray.TYPE
+		mov		rax,	qwordArray.LENGTHOF
+		mov		rax,	[rsi]
 
-		mov		rax, 60					
-		mov		rdi, 0					
+		mov		rdi,	rax
+		mov		rax, 	60					
 		syscall							
